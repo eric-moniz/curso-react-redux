59d65aff2a726c135821d8f6e3c2a4e22ce9dfe3
Eric Moniz <ericmoniz@outlook.com.br>
2023-03-13 19:56:21 -0300

    aula 25 - Componente Card #03

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,35 @@ const App = () => (
     <h1>Fundamentos React</h1>
 
     <div className='Cards'>
-      <Card titulo='#04 - Desafio Aleatório'>
+      <Card
+        titulo='#04 - Desafio Aleatório'
+        color='#fa6900'
+      >
         <Aleatorio
           max={20}
           min={10}
         />
       </Card>
-      <Card titulo='#03 - Fragmento'>
+      <Card
+        titulo='#03 - Fragmento'
+        color='#e94c6f'
+      >
         <Fragmento />
       </Card>
-      <Card titulo='#02 - Com Parâmetro'>
+      <Card
+        titulo='#02 - Com Parâmetro'
+        color='#e8b71a'
+      >
         <ComParametro
           titulo='Situação Do Aluno'
           aluno='João'
           nota={6.9}
         />
       </Card>
-      <Card titulo='#01 - Primeiro Componente'>
+      <Card
+        titulo='#01 - Primeiro Componente'
+        color='#588c73'
+      >
         <Primeiro />
       </Card>
     </div>
