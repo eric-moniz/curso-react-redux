59db88bdc22b7fe46f3568c570e224478da28852
Eric Moniz <ericmoniz@outlook.com.br>
2023-03-13 20:26:58 -0300

    aula 26 - Componente com Filho #01

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,32 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametros';
-import Fragmento from './components/basicos/Fragmento';
-import Aleatorio from './components/basicos/Aleatorio';
-import Card from './components/basicos/layout/Card';
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametros";
+import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/basicos/layout/Card";
+import Familia from "./components/basicos/Familia";
 
 const App = () => (
-  <div className='App'>
+  <div className="App">
     <h1>Fundamentos React</h1>
 
-    <div className='Cards'>
-      <Card
-        titulo='#04 - Desafio Aleatório'
-        color='#fa6900'
-      >
-        <Aleatorio
-          max={20}
-          min={10}
-        />
+    <div className="Cards">
+      <Card titulo="#05 - Componente com Filhos" color="#00c8f8">
+        <Familia sobrenome="Ferreira" />
       </Card>
-      <Card
-        titulo='#03 - Fragmento'
-        color='#e94c6f'
-      >
+
+      <Card titulo="#04 - Desafio Aleatório" color="#fa6900">
+        <Aleatorio max={20} min={10} />
+      </Card>
+      <Card titulo="#03 - Fragmento" color="#e94c6f">
         <Fragmento />
       </Card>
-      <Card
-        titulo='#02 - Com Parâmetro'
-        color='#e8b71a'
-      >
-        <ComParametro
-          titulo='Situação Do Aluno'
-          aluno='João'
-          nota={6.9}
-        />
+      <Card titulo="#02 - Com Parâmetro" color="#e8b71a">
+        <ComParametro titulo="Situação Do Aluno" aluno="João" nota={6.9} />
       </Card>
-      <Card
-        titulo='#01 - Primeiro Componente'
-        color='#588c73'
-      >
+      <Card titulo="#01 - Primeiro Componente" color="#588c73">
         <Primeiro />
       </Card>
     </div>
